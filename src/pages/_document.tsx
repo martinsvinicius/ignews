f50a546f690a645768e13b6b9e1e026fb6e80625
Vinicius Martins <vinicius.martins@parnamirim.rn.gov.br>
2021-05-26 11:12:48 -0300

    global styles

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,8 +7,6 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"/>
-
-          <title>ig.news</title>
         </Head>
         <body>
           <Main />
