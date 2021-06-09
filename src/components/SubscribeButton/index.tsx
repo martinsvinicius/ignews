009f842e80e7b00f0065959f11731d49cd694f22
Vinicius Victor <vinicius.victor.sm@gmail.com>
2021-06-09 09:19:07 -0300

    checkout session generation

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,8 +1,30 @@
+import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
 import styles from './styles.module.scss';
 
 export function SubscribeButton() {
+  const [session] = useSession();
+
+  async function handleSubscribe() {
+    if(!session) {
+      signIn('github');
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+    } catch {
+
+    }
+  }
+
   return(
-    <button className={styles.subscribeButton} type="button">
+    <button 
+      className={styles.subscribeButton} 
+      type="button"
+      onClick={handleSubscribe}>
       Subscribe now
     </button>
   );
