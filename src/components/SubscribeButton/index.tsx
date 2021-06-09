2bd7711e4b0e4c1c918e4a745f8134538d8ef056
Vinicius Victor <vinicius.victor.sm@gmail.com>
2021-06-09 09:49:02 -0300

    stripe checkout session; toastify

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,14 @@
 import { signIn, useSession } from 'next-auth/client';
+import { toast } from 'react-toastify';
 import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 export function SubscribeButton() {
   const [session] = useSession();
 
   async function handleSubscribe() {
-    if(!session) {
+    if (!session) {
       signIn('github');
       return;
     }
@@ -15,16 +17,21 @@ export function SubscribeButton() {
       const response = await api.post('/subscribe');
 
       const { sessionId } = response.data;
-    } catch {
 
+      const stripejs = await getStripeJs();
+
+      await stripejs.redirectToCheckout({sessionId});
+    } catch (err) {
+      toast.error(err.message);
     }
   }
 
-  return(
-    <button 
-      className={styles.subscribeButton} 
+  return (
+    <button
+      className={styles.subscribeButton}
       type="button"
-      onClick={handleSubscribe}>
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,15 @@ import { Header } from '../components/Header';
 import { Provider as NextProvider } from 'next-auth/client';
 
 import '../styles/global.scss';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
+      <ToastContainer />
     </NextProvider>
   );
 }
