200db14ee83b5fd5c891f4f0d56f131e260b9a51
Vinicius Martins <vinicius.victor.sm@gmail.com>
2021-07-05 20:35:56 -0300

    working with CMS

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,23 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+
+import Prismic from '@prismicio/client';
+import { getPrismicClient } from '../../services/prismic';
+import { RichText } from 'prismic-dom';
+
 import styles from './styles.module.scss';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -10,40 +26,49 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de abril de 2021</time>
-            <strong>My bday :)</strong>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-              maximus dignissim neque, non maximus odio blandit at. Aliquam
-              sapien libero, bibendum tristique dignissim sed, auctor vel arcu.
-              Curabitur sed sodales ipsum, sit amet vulputate eros.
-            </p>
-          </a>
-
-          <a href="#">
-            <time>12 de abril de 2021</time>
-            <strong>My bday :)</strong>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-              maximus dignissim neque, non maximus odio blandit at. Aliquam
-              sapien libero, bibendum tristique dignissim sed, auctor vel arcu.
-              Curabitur sed sodales ipsum, sit amet vulputate eros.
-            </p>
-          </a>
-
-          <a href="#">
-            <time>12 de abril de 2021</time>
-            <strong>My bday :)</strong>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-              maximus dignissim neque, non maximus odio blandit at. Aliquam
-              sapien libero, bibendum tristique dignissim sed, auctor vel arcu.
-              Curabitur sed sodales ipsum, sit amet vulputate eros.
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a key={post.slug} href={`#${post.slug}`}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title', 'post.content'],
+      pageSize: 100,
+    }
+  );
+
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === 'paragraph')
+          ?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        'pt-BR',
+        {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        }
+      ),
+    };
+  });
+
+  return {
+    props: { posts },
+  };
+};
