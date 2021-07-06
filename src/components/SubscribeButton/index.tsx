5dea3cc27c1e6c2f07e9a3ce43f8755e66bc4eea
Vinicius Martins <vinicius.victor.sm@gmail.com>
2021-07-06 00:37:24 -0300

    post page and subscriptions validations

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { signIn, useSession } from 'next-auth/client';
+import { useRouter } from 'next/dist/client/router';
 import { toast } from 'react-toastify';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
@@ -6,10 +7,16 @@ import styles from './styles.module.scss';
 
 export function SubscribeButton() {
   const [session] = useSession();
+  const router = useRouter();
 
   async function handleSubscribe() {
     if (!session) {
       signIn('github');
+      return;
+    }
+
+    if (session.activeSubscription) {
+      router.push('/posts');
       return;
     }
 
@@ -20,7 +27,7 @@ export function SubscribeButton() {
 
       const stripejs = await getStripeJs();
 
-      await stripejs.redirectToCheckout({sessionId});
+      await stripejs.redirectToCheckout({ sessionId });
     } catch (err) {
       toast.error(err.message);
     }
