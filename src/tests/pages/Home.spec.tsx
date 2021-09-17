29b6f5cf5b32cf6934956d2af7e167f2eb72481e
vinicius5g <vinicius.victor.sm@gmail.com>
2021-09-16 21:42:12 -0300

    testing if Home page loads initial data

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
+import { mocked } from 'ts-jest/utils';
 
-import Home from '../../pages';
+import Home, { getStaticProps } from '../../pages';
+import { stripe } from '../../services/stripe';
 
 jest.mock('next/router');
 jest.mock('next-auth/client', () => {
@@ -8,11 +10,34 @@ jest.mock('next-auth/client', () => {
     useSession: () => [null, false],
   };
 });
+jest.mock('../../services/stripe');
 
 describe('Home page', () => {
   it('renders correctly', () => {
-    render(<Home product={{ priceId: 'fake-price-id', amount: 'R$10,00' }} />);
+    render(<Home product={{ priceId: 'fake-price-id', amount: '$10,00' }} />);
 
-    expect(screen.getByText('for R$10,00 month')).toBeInTheDocument();
+    expect(screen.getByText('for $10,00 month')).toBeInTheDocument();
+  });
+
+  it('loads initial data', async () => {
+    const retrieveStripeMocked = mocked(stripe.prices.retrieve);
+
+    retrieveStripeMocked.mockResolvedValueOnce({
+      id: 'fake-price-id',
+      unit_amount: 1000,
+    } as any);
+
+    const response = await getStaticProps({});
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          product: {
+            priceId: 'fake-price-id',
+            amount: '$10.00',
+          },
+        },
+      })
+    );
   });
 });
